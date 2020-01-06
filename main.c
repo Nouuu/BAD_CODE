16be50be3352e2e0e19885ead3cc6543c5cc74f5
Noù <nlarrieulacoste@myges.fr>
2020-01-06 15:54:00 +0100

    GTK with Glade

--- main.c
+++ main.c
@@ -1,18 +1,64 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sqlite3.h>
+#include <gtk-3.0/gtk/gtk.h>
 
 //#include "headers/sqlite/sqlite3.h"
 #include "headers/sqliteFunctions.h"
 
-int main(void) {
-    checkVersion();
-    sqlite3 *db;
+void createGTKWindows(int *argc, char ***argv);
+void OnDestroy();
 
-    connectDB("tests.db", &db);
-    getAllStudents(db);
+int main(int argc, char **argv) {
+//    checkVersion();
+//    sqlite3 *db;
+//
+//    connectDB("tests.db", &db);
+//    getAllStudents(db);
+//    sqlite3_close(db);
+    createGTKWindows(&argc, &argv);
 
-    sqlite3_close(db);
+    return EXIT_SUCCESS;
+}
 
-    return 0;
+void createGTKWindows(int *argc, char ***argv) {
+
+    GtkWidget *pWindow;
+    GtkBuilder *builder = NULL;
+    GError *error = NULL;
+    gchar *filename = NULL;
+
+    gtk_init(argc, argv);
+
+    builder = gtk_builder_new();
+
+    filename = g_build_filename("test.glade", NULL);
+
+    gtk_builder_add_from_file(builder, filename, &error);
+    g_free(filename);
+
+    if (error)
+    {
+        gint code = error->code;
+        g_printerr("%s\n", error->message);
+        g_error_free (error);
+        exit(code);
+    }
+
+    pWindow = GTK_WIDGET(gtk_builder_get_object (builder, "Bad Code"));
+
+//    g_signal_connect (G_OBJECT (pWindow), "destroy", G_CALLBACK(OnDestroy), NULL);
+    g_signal_connect (G_OBJECT (pWindow), "destroy", (GCallback)gtk_main_quit, NULL);
+
+
+    gtk_widget_show_all(pWindow);
+
+    gtk_main();
+}
+
+void OnDestroy()
+{
+    /* Arret de la boucle evenementielle */
+    gtk_main_quit();
 }
