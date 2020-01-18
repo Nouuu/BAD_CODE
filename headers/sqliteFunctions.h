cbe8dabe09f83fbc2851c391c2d6f49039ed8853
Nouu <noelarrieulacoste@yahoo.fr>
2020-01-18 15:29:50 +0100

    insertUserImage function v1

--- headers/sqliteFunctions.h
+++ headers/sqliteFunctions.h
@@ -53,5 +53,66 @@ void getAllStudents(sqlite3 *db){
     fprintf(stdout, "Nombre d'eleves : %d", rows);
 }
 
+int insertUserImage(char *dbname, int userId, char *photo_location) {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////    PARTIE FICHIER /////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    FILE *file = fopen(photo_location, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "Cannot open image\n");
+        return 1;
+    }
+
+    fseek(file, 0, SEEK_END);
+    int flen = ftell(file);
+    if (flen == -1) {
+        fprintf(stderr, "Cannot get filesize");
+        fclose(file);
+        return 1;
+    }
+    fseek(file, 0, SEEK_SET);
+
+    char *data = malloc(flen + 1);
+    unsigned long long size = fread(data, sizeof(char), flen, file);
+    //data contient maintenant tout le binaire du fichier, on peut donc fermer le fichier
+
+    int r = fclose(file);
+    if (r == EOF) {
+        fprintf(stderr, "Cannot close file handler\n");
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////    PARTIE SQL /////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    sqlite3 *db = connectDB(dbname);
+    sqlite3_stmt *pStmt;
+    char *sqlRequest = "UPDATE USER SET photo = ? WHERE id = ?";
+
+    int returnCode = sqlite3_prepare_v2(db, sqlRequest, (int) strlen(sqlRequest), &pStmt, NULL);
+    if (returnCode != SQLITE_OK) {
+        fprintf(stderr, "Cannot prepare sql request stattement: %s\n", sqlite3_errmsg(db));
+        return 1;
+    }
+
+    sqlite3_bind_blob(pStmt, 1, data, (int) size, SQLITE_STATIC);
+    //remplace le parametre 1 ('?' n°1) par un blob
+    sqlite3_bind_int(pStmt, 2, userId);
+    //remplace le parametre 2 ('?' n°2) par un entier
+
+    returnCode = sqlite3_step(pStmt);
+    if (returnCode != SQLITE_DONE) {
+
+        printf("execution failed: %s", sqlite3_errmsg(db));
+    }
+
+    free(data);
+    sqlite3_finalize(pStmt);
+    sqlite3_close(db);
+    return 0;
+}
+
 
 #endif //BAD_CODE_SQLITEFUNCTIONS_H
