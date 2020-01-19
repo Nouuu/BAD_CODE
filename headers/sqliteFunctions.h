733bd2c0103ccfbad1e97dc879569dce51edd204
Nouu <noelarrieulacoste@yahoo.fr>
2020-01-19 13:29:07 +0100

    remove old function list student

--- headers/sqliteFunctions.h
+++ headers/sqliteFunctions.h
@@ -21,38 +21,6 @@ sqlite3 *connectDB(char *dbname) {
     return db;
 }
 
-void getAllStudents(sqlite3 *db) {
-    sqlite3_stmt *request;
-    char *sqlSelectStudents = "SELECT * FROM students";
-    int id = 0, rows = 0;
-    char *first_name = NULL;
-    char *last_name = NULL;
-    char *class = NULL;
-    int returnCode;
-
-    fprintf(stdout, "Recuperation de tout les eleves\n");
-
-    returnCode = sqlite3_prepare_v2(db, sqlSelectStudents, (int) strlen(sqlSelectStudents), &request, NULL);
-    if (!returnCode) {
-        while (returnCode == SQLITE_OK || returnCode == SQLITE_ROW) {
-
-            returnCode = sqlite3_step(request);
-            if (returnCode == SQLITE_OK || returnCode == SQLITE_ROW) {
-
-                id = sqlite3_column_int(request, 0);
-                first_name = (char *) sqlite3_column_text(request, 1);
-                last_name = (char *) sqlite3_column_text(request, 2);
-                class = (char *) sqlite3_column_text(request, 3);
-
-                fprintf(stdout, "| %d\t| %s\t| %s\t| %s\t|\n", id, first_name, last_name, class);
-                rows++;
-            }
-        }
-        sqlite3_finalize(request);
-    }
-    fprintf(stdout, "Nombre d'eleves : %d", rows);
-}
-
 int insertUserImage(char *dbname, int userId, char *photo_location) {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
